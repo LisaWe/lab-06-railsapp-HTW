c3713860013f0d2b8532839c1f941dcacbfe43c2
LisaWe <l.wechmann@gmx.net>
2016-01-08 12:09:09 +0100

    Added three pumpkins.

---
+++ spec/factories/superHokkaido.rb
@@ -0,0 +1,19 @@
+FactoryGirl.define do
+  factory :spicyHokkaido, class: Pumpkin do
+    name 'spicyHokkaido'
+    color 'blood-red'
+    size  'XL'
+    price '7.25'
+  end
+  factory :fruityHokkaido, class: Pumpkin do
+    name 'fruityHokkaido '
+    color 'white, red-dotted'
+    size  'S'
+    price '10.0'
+  end
+  factory :cryspyHokkaido, class: Pumpkin do
+    name 'cryspyHokkaido'
+    color 'caramel-brown'
+    size  'XXS'
+    price '0.33'
+  end
